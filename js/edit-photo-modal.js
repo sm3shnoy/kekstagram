630e1f1e8f22c6fc2283966ad14349d93695b610
Smirnov Alex <aasmirnov@tsum.ru>
2023-02-02 20:31:16 +0300

    adds upload photo

--- js/edit-photo-modal.js
+++ js/edit-photo-modal.js
@@ -1,15 +1,17 @@
 import { clearFilter } from './add-effects.js';
 import { isEscEvent, isEnterEvent } from './util.js';
 import { uploadPhotoForm } from './new-photo-form.js';
+import { uploadFileField } from './upload-photo.js';
 
 const DEFAULT_ZOOM_VALUE = 100;
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
 
 const body = document.querySelector('body');
-const uploadFileField = document.querySelector('#upload-file');
 const uploadModalOverlay = document.querySelector('.img-upload__overlay');
 const uploadModalCloseButton = document.querySelector('#upload-cancel');
 const zoomValue = document.querySelector('.scale__control--value');
 const preview = document.querySelector('.img-upload__preview img');
+const effectsPreview = document.querySelectorAll('.effects__preview');
 
 export const onUploadModalEscKeydown = (evt) => {
   if (isEscEvent(evt)) {
@@ -31,6 +33,24 @@ export const uploadModalOpen = () => {
   body.classList.add('modal-open');
   document.addEventListener('keydown', onUploadModalEscKeydown);
   uploadModalCloseButton.addEventListener('click', uploadModalClose);
+
+  const file = uploadFileField.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((item) => fileName.endsWith(item));
+
+  if (matches) {
+    const reader = new FileReader();
+
+    reader.addEventListener('load', () => {
+      preview.src = reader.result;
+      effectsPreview.forEach((item) => {
+        item.style.backgroundImage = `url("${reader.result}")`;
+      });
+    });
+
+    reader.readAsDataURL(file);
+  }
 };
 
 export const uploadModalClose = () => {
